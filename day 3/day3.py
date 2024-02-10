e5736a2bb6a97b1c4a2982a1f83e5e4b9cfab9ab
imedashvililizi <liziimedashviliG59@gmail.com>
2024-02-10 15:53:28 +0400

    commit

---
+++ day 3/day3.py
@@ -0,0 +1,12 @@
+name0 = "ia "
+surname0 = "futkarashvili"
+print(name0 + surname0)
+name1 = "imeda "
+surname1 = "imedashvili"
+print(name1 + surname1)
+name2 = "tazo "
+surname2 = "imedashvili"
+print(name2 + surname2)
+name3 = "lile"
+surname3 ="imedashvili"
+print(name3,surname3)
