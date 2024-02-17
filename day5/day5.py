1961cab79ae47aefdf599cefb7a9ada78178a3e7
imedashvililizi <liziimedashviliG59@gmail.com>
2024-02-17 22:58:22 +0400

    commit

---
+++ day5/day5.py
@@ -0,0 +1,105 @@
+
+#book price
+the_price_of_the_book1=30
+#book discount
+discount=10
+#price after discount
+how_much_is_the_discount=the_price_of_the_book1*discount/100
+#book price after sale
+new_price=(the_price_of_the_book1-how_much_is_the_discount)
+print(new_price)
+
+#book price
+the_price_of_the_book2=35
+#book discount
+discount=10
+#price after discount
+how_much_is_the_discount=the_price_of_the_book2*discount/100
+#book price after sale
+new_price=(the_price_of_the_book2-how_much_is_the_discount)
+print(new_price)
+
+#book price
+the_price_of_the_book3=50
+#book discount
+discount=10
+#price after discount
+how_much_is_the_discount=the_price_of_the_book3*discount/100
+#book price after sale
+new_price=(the_price_of_the_book3-how_much_is_the_discount)
+print(new_price)
+
+#book price
+the_price_of_the_book4=56
+#book discount
+discount=10
+#price after discount
+how_much_is_the_discount=the_price_of_the_book4*discount/100
+#book price after sale
+new_price=(the_price_of_the_book4-how_much_is_the_discount)
+print(new_price)
+
+#book price
+the_price_of_the_book5=22
+#book discount
+discount=10
+#price after discount
+how_much_is_the_discount=the_price_of_the_book5*discount/100
+#book price after sale
+new_price=(the_price_of_the_book5-how_much_is_the_discount)
+print(new_price)
+
+
+#book price
+the_price_of_the_book6=60
+#book discount
+discount=20
+#price after discount
+how_much_is_the_discount=the_price_of_the_book6*discount/100
+#book price after sale
+new_price=(the_price_of_the_book6-how_much_is_the_discount)
+print(new_price)
+
+
+#book price
+the_price_of_the_book7=75
+#book discount
+discount=20
+#price after discount
+how_much_is_the_discount=the_price_of_the_book7*discount/100
+#book price after sale
+new_price=(the_price_of_the_book7-how_much_is_the_discount)
+print(new_price)
+
+
+#book price
+the_price_of_the_book8=100
+#book discount
+discount=20
+#price after discount
+how_much_is_the_discount=the_price_of_the_book8*discount/100
+#book price after sale
+new_price=(the_price_of_the_book8-how_much_is_the_discount)
+print(new_price)
+
+
+#book price
+the_price_of_the_book9=120
+#book discount
+discount=20
+#price after discount
+how_much_is_the_discount=the_price_of_the_book9*discount/100
+#book price after sale
+new_price=(the_price_of_the_book9-how_much_is_the_discount)
+print(new_price)
+
+
+#book price
+the_price_of_the_book10=98
+#book discount
+discount=20
+#price after discount
+how_much_is_the_discount=the_price_of_the_book10*discount/100
+#book price after sale
+new_price=(the_price_of_the_book10-how_much_is_the_discount)
+print(new_price)
