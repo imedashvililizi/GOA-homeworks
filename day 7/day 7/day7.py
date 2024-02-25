af93ca1a77ac8a5c20b5c4a7aa09ffd03bb7fa39
imedashvililizi <liziimedashviliG59@gmail.com>
2024-02-25 18:16:56 +0400

    commit

---
+++ day 7/day 7/day7.py
@@ -0,0 +1,15 @@
+num1=int(input("enter the fitst number: "))
+num2=int(input("enter the second number: "))
+
+print(float(num1) + 8)
+print(str(num2) + "3")
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 * num2)
+
+print(num2 > num1)
+print(num2 < num1)
+print(num2 == num1)
