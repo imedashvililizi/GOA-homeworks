778cc68e03527786a371ab06dbf324e20450667b
imedashvililizi <liziimedashviliG59@gmail.com>
2024-03-07 22:55:35 +0400

    commit

---
+++ day 10/day10.py
@@ -0,0 +1,62 @@
+i=0 
+
+for i in range(15):
+    print(i)
+
+ 
+
+for i in range(30):
+    if i % 2 == 0:
+        print(i, "is even")
+    
+
+
+    
+i=1
+
+for i in range(25):
+    print((i + 7) / 2)
+    
+
+i=1
+
+for i in range(20):
+    print((i + 7) / 2)
+
+
+i=20
+
+for i in range(40):
+    print(i * 2)
+
+
+
+
+for i in range(7):
+    print("lizi")
+
+
+i=0
+
+for i in range(10):
+    print((i * 2) * 3)
+
+
+
+    i=0
+
+for i in range(5):
+    print((i * 10) / 5)
+
+
+    i=0
+
+for i in range(15):
+    print((i - 3) / 3)
+          
+
+
+i=0
+
+for i in range(18):
+    print(i + 2,5)
