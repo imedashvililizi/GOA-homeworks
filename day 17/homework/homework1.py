55d691639a2cd4876f387f6fa83a4063ebfe9ce9
imedashvililizi <liziimedashviliG59@gmail.com>
2024-03-29 23:55:33 +0400

    commit

---
+++ day 17/homework/homework1.py
@@ -0,0 +1,7 @@
+""" შექმენით ლისტი სადაც ჩამოწერილიქნება თამაშის სახელები და შემდგომ თამაშებს ვანაცვლებთ სოლოლერნით
+ , w3 , codewars - ებით და ასეშემდეგ."""
+games = ["pubg","stalker","minecraft"]
+games[0] = "visual studio"
+games[1] = "codewars"
+games[2] = "w3schools"
+print(games[0],games[1],games[2])
