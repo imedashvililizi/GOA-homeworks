41059c0d29069744dc8d1ce5af4c46af9a4439d7
imedashvililizi <liziimedashviliG59@gmail.com>
2024-04-12 00:40:07 +0400

    commit

---
+++ day 20/homework/homework2.py
@@ -0,0 +1,6 @@
+"""We need a function that can transform
+ a number (integer) into a string."""
+def number_to_string(num):
+    # Return a string of the number here!
+    num = str(num)
+    return num
---
+++ day 20/homework/homework5.py
@@ -0,0 +1,5 @@
+"""Code as fast as you can! 
+You need to double the integer and return it"""
+def double_integer(i):
+    pass # Double the integer and return it!
+    return i * 2
