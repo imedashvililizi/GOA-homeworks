fcc420b22c44f30deb295e08581d0241b85ae3eb
imedashvililizi <liziimedashvilig59@gmail.com>
2024-06-10 23:48:54 +0400

    commit

---
+++ day 36/claswork/cw1.py
@@ -0,0 +1,62 @@
+
+#თუ არ იწყება დიდი სიმბოლოთი ადიდებს პირველ სიმბოლოს
+# name = (input("enter name:"))
+# surname = (input("enter your surname:"))
+# print(name.capitalize())
+# print(surname.capitalize())
+
+#ითვლის რამდენი a არის სტრინგში
+# Proposal = input("enter preposal")
+# print(Proposal.count("a"))
+
+
+#ითივის სტრინგში რამდენჯერ არის სიმბოლო a თუ არ არის გამოაქვს ერორი
+# name=input("enter name:")
+# print(name.index("a"))
+
+
+#ითივის სტრინგში რამდენჯერ არის სიმბოლო a თუ არ არის გამოაქვს -1
+# name=input("enter name:")
+# print(name.find("a"))
+
+#ამოწმებს შეიცავს თარა სთრინგი მხოლოდ სიმბოლოებს და რიცხვებს
+# name = "lizi234"
+# print(name.isalnum())
+
+#ამოწმებს შეიცავს თუარა სტრინგი მხოლოდ სიმბოლოებს
+# name = "lizi0000"
+# print(name.isalpha())
+
+#ამოწმებს სტრინგი შეიცავს თუარა მხოლოდ რიცხვებს
+# name = "0000"
+# print(name.isdigit())
+
+#ამოწმებს სტრინგი თუ არის დაწერილი დიდი სიმბოლოებით
+# surname=("IMEDASHVILI")
+# print(surname.isupper())
+
+#ამოწმებს სტრინგი თუ არის დაწერილი პატარა სიმბოლოებით
+# name=("lizi")
+# print(name.islower())
+
+#ცვლის სტრინგშ სიმბოლო a-ს სიმბოლო k_თი
+# name=input("enter name:")
+# print(name.replace("a" , "k"))
+
+#ამოწმებს იწყებატუარა სტრინგი დიდი l-ით და თუ მთავრდება პატარა e-თი
+# name="lizi"
+# print(name.startswith("l"))
+# print(name.endswith("e"))
+
+# name_and_surname=input("enter your name and surname")
+# print(name_and_surname.split(" "))
+
+
+
+
+
+
+
+
+
+
---
+++ day 36/hw1.py
@@ -0,0 +1,51 @@
+#1 append ამათებს სტრინგს სიაში
+# list=[]
+# list.append("lizi")
+# list.append("ani")
+# list.append("lile")
+# print(list)
+
+#2 ასუფთავებს მთლიან სიას
+# list =["lizi","ani","salome"]
+# list.clear()
+# print(list)
+
+#3 აკოპირებს სიას და ინახავს მეორე ცვლადში
+# list=["lizi","ani","salome"]
+# list1 =list.copy()
+# print(list1)
+
+
+#4 გვითვლის სასურველი სახელი რამდენჯერარი ლისთში
+# list=["lizi","ani","lizi"]
+# print(list.count("lizi"))
+
+#5 ადგენს რომელ ინდექსზეა სასურველი სახელი
+# list=["lizi","ani","mari"]
+# print(list.index("lizi"))
+
+#6 ამათებს სიაშ ახალ მესამე ინდექსს ლუკას
+# list=["lizi","ani","mari"]
+# list.insert(3,"luka")
+# print(list)
+
+#7 შლის კონკრეტულ სახელს ლისტში
+# list=["lizi","ani","mari"]
+# list.remove("lizi")
+# print(list)
+
+#8 აბრუნებს ლისტს
+# list=["lizi","ani","mari"]
+# list.reverse()
+# print(list)
+
+#9 ალაგებს ზრდადობით რიცხვებსბ
+# list=[7,3,6,9]
+# list.sort()
+# print(list)
+
+#10 აფართოვებს ლისტს
+# list1=[2,5,9,10]
+# list=[3,7,33,44]
+# list1.extend(list)
+# print(list1)
