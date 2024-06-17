e07dd6c74baa20a8d37d4e27f6ef31cb5e4b1ea2
imedashvililizi <liziimedashvilig59@gmail.com>
2024-06-18 01:35:07 +0400

    commit

---
+++ day 38/homework/hw.py
@@ -0,0 +1,13 @@
+"""შექმენით dictionary რმელსაც დაარქმევდთ student და გადასცემთ შესაბამის key და value"""
+student = {
+    "name":"lizi",
+    "surname":"imedashvili",
+    "course":"2"
+}
+"""შექმენით ცვლადი რომელსაც დავარქმევთ result და for ციკლის გამოყენებით results დავუმატებთ student vales და დავპრინტავთ"""
+
+result=[]
+for i in range(student):
+    result.append(i)
+
+print(result)
